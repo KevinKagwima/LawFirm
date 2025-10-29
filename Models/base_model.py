ce6b588103df91fcffc39708585ab8a85fe7fe33
kevokagwima <kevokagwima@gmail.com>
2025-10-29 12:11:28 +0300

    updated files

--- Models/base_model.py
+++ Models/base_model.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 bcrypt = Bcrypt()
 
 def get_local_time():
-  utc_timezone = datetime.now(pytz.utc)
+  utc_timezone = datetime.now(pytz.utc) + timedelta(hours=3)
   local_tz = pytz.timezone('Africa/Nairobi')
   return utc_timezone.astimezone(local_tz)
 
